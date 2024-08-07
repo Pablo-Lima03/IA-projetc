aa50047db73dcecd0242fe102996b024469087a6
Pablo-Lima03 <128814333+Pablo-Lima03@users.noreply.github.com>
2024-08-07 09:08:43 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,97 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        alternativas: [
+            {
+                texto: "Isso é assustador!",
+                afirmacao: " "
+            },
+            {
+                texto: "Isso é maravilhoso!",
+                afirmacao: " "
+            }
+        ]
+    },
+    {
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Pesquisaria a respeio sobre IA's no geral, tanto como sua funcionalidade como seu uso em classe, após isso, pesquisaria até por IA's suas vantagens e desvantagens na sala de aula "
+              
+            },
+            
+        ]
+    },
+    {
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "As IA's em geral irão automatizar o serviço e processos no meio de trabalho, tendo em risco de grande parte da população perder seus emoregos, como medida poderiam capacitar esses trabalhadores, com cursoso, especializações e graduações a fim de elevar sua experiencia e proporcinar oportunidade dos mesmos, no mercado de trabalho futuro.  "              
+            },
+            
+        ]
+    },
+    {
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
+            {
+                texto: "Iria descrever uma imagem onde, em um futuro próximo, a tecnologia e a humanidade coexistam, e a sociedade use as IA's de maneira benefica ",
+                        }                    ]
+    },
+    {
+        enunciado: "Você tem um trabalho em grupo de matemática para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
+            {
+                texto: " Geralmente, pegaria o texto e jogaria em alguma iA que formateasss o texto, de forma que mudasse as palavras, mas sem comprometer o contexto e sentido do texto em si",
+                
+            },
+            
+        ]
+    },
+];
+
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "O futuro nos aguarda";
+}
+
+mostraPergunta();
